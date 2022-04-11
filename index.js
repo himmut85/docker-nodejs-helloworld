50deab9a0686ce9e1b7fafd95e9a7f73f60a1de0
himmut85 <67488800+himmut85@users.noreply.github.com>
2022-04-12 00:19:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 
 // App
 var app = express();
-app.get('/run1', function (req, res) {
-  res.send('cloud run - Hello World  - run1 \n');
+app.get('/master1', function (req, res) {
+  res.send('cloud run - Hello World  - master1 \n');
 });
 
-app.get('/run2', function (req, res) {
-  res.send('cloud run - Hello World  - run2 \n');
+app.get('/master2', function (req, res) {
+  res.send('cloud run - Hello World  - master2 \n');
 });
 
-app.get('/run3', function (req, res) {
-  res.send('cloud run - Hello World  - run3 \n');
+app.get('/master3', function (req, res) {
+  res.send('cloud run - Hello World  - master3 \n');
 });
 
 app.listen(PORT)
